6269204d6876a03c29e6da6311b27a9982a817b9
Walter Junior <walter.junior@endurance.com>
2019-08-06 11:23:56 -0300

    Implementação da busca por jogadores

--- main.go
+++ main.go
@@ -4,13 +4,12 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/WalterPaes/ManUtdApi/player"
 	"github.com/WalterPaes/ManUtdApi/team"
 )
 
 func main() {
 	http.HandleFunc("/", team.Handler)
-	http.HandleFunc("/player/", player.Handler)
+	http.HandleFunc("/player/", team.Handler)
 	log.Println("Executando...")
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
--- team/team.go
+++ team/team.go
@@ -5,18 +5,31 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 
 	"github.com/WalterPaes/ManUtdApi/player"
 )
 
 type Team struct {
-	Player []player.Player
+	Player []player.Player `json:"player"`
 }
 
 func Handler(writer http.ResponseWriter, request *http.Request) {
+
 	switch {
 	case request.Method == "GET":
-		response := GetPlayers()
+		var response string
+		var err error
+		player := strings.TrimPrefix(request.URL.Path, "/player/")
+		if len(player) > 0 {
+			response, err = GetPlayer(player)
+		} else {
+			response, err = GetPlayers()
+		}
+
+		if err != nil {
+			panic(err.Error())
+		}
 
 		writer.Header().Set("Content-Type", "application/json")
 		fmt.Fprint(writer, response)
@@ -26,26 +39,60 @@ func Handler(writer http.ResponseWriter, request *http.Request) {
 	}
 }
 
-func GetPlayers() string {
+func GetPlayers() (string, error) {
 	res, err := http.Get("https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?t=Man%20United")
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
 	defer res.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
 	var players Team
-	json.Unmarshal(body, &players)
+	err = json.Unmarshal(body, &players)
+	if err != nil {
+		return "", err
+	}
 
 	json, err := json.Marshal(players)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
-	return string(json)
+	return string(json), err
+}
+
+func GetPlayer(name string) (string, error) {
+
+	name = strings.ReplaceAll(name, " ", "%20")
+	url := fmt.Sprintf("https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?p=%s", name)
+
+	res, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	var player Team
+	err = json.Unmarshal(body, &player)
+	if err != nil {
+		return "", err
+	}
+
+	json, err := json.Marshal(player)
+	if err != nil {
+		return "", err
+	}
+
+	return string(json), err
 }
